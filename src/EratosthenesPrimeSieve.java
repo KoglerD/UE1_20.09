f14ddc877dcf8d748f20d27c777c8b76ba92e55c
KoglerD <koglerd190073@sus.htl-grieskirchen.at>
2021-09-20 21:52:55 +0200

    Methode isPrime from Row 13 to 34 but doesnt work

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -1,7 +1,35 @@
+import java.util.ArrayList;
+
 public class EratosthenesPrimeSieve implements PrimeSieve{
+
+    private int upperNumber;
+    private ArrayList<Integer> primeList;
+
+    public EratosthenesPrimeSieve(int upperNumber) {
+        this.upperNumber = upperNumber;
+    }
 
     @Override
     public boolean isPrime(int p) {
+        if (p == 2){
+            return true;
+        }
+
+        for (int i = 3; i < (p/2); i++) {
+            for (int j = 2; j < p/2; j++) {
+                if (p > 2){
+                    /*if ((i^j) <= (p/2)){
+
+                    }
+                    if ((p/i) != (){
+
+                    }*/
+                }
+            }
+
+        }
+
+
         return false;
     }
 
