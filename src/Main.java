8badc4438c4498c5690243ce12f2429503fc347e
KoglerD <koglerd190073@sus.htl-grieskirchen.at>
2021-09-26 19:23:29 +0200

    UI hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 import java.time.chrono.Era;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve(132);
-        eps.isPrime(232);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Geben Sie eine Obergrenze ein: ");
+        String upperN = scanner.nextLine();
+        int upperNumber = Integer.parseInt(upperN);
+        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve(upperNumber);
+        System.out.println("Bei welcher Zahl wollen Sie wissen, ob es eine Primzahl ist?!");
+        String isP = scanner.nextLine();
+        int isPrim = Integer.parseInt(isP);
+        eps.isPrime(isPrim);
         eps.printPrimes();
         eps.printPrimeSum();
     }
